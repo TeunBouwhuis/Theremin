30ff979a30df59e9a9a9d16996be5c69f1cb3f67
TeunBouwhuis <teunbouwhuis132@gmail.com>
2025-10-17 14:53:31 +0200

    Lees vorume regelaar met interrupt
    Leest volume regelaar 0 tot 255 met serial debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,42 @@
-/*
- * generate a 440Hz tone, i.e. interrupts at 880Hz
- * Connect speaker to pin 3
- */
-#include <avr/interrupt.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <Arduino.h> // VOOR SERIAL EN DEBUG WEGHALEN IN TOEKOMST (DENK IK)
 
-/*
- * prototypes
- */
-void setup_timer0(void);
+volatile uint8_t DRAAI_DING_WAARDE = 0;
+volatile uint8_t LEES_DATA = 0;
 
-/*
- * COMPA timer0
- */
-ISR(TIMER0_COMPA_vect) {
-  PORTD ^= (1<<PD3);      // toggle
-}
-
-/*
- * main
- */
-int main(void){
-  // setup
-  DDRD |= (1<<PD3); // pin 3 tone
-  setup_timer0();
-
-  // endless loop
-  while(1) {
-                    // empty
-  }
-
-  return(0);        // never reached
-}
-
-/*
- * timer0 stuff
- * 880Hz => 0,001136s period
- * COM0x[1:0] = 0b00 (pin OC0x disconnected)
- * WGM0[2:0] = 0b010 (mode 2: CTC)
- * Fint = Fcpu / (N * (OCR0A+1)) <=>
- * OCR0A = Fcpu / (N * Fint) - 1
- * OCR0A = 16000000 / (N * 880) - 1
- *    = 18180 (with N=1)
- *    = 2271 (with N=8)
- *    = 263 (with N=64)
- *    = 70 (with N=256)!
- * CS0[2:0] = 0b100 (1/256 prescale)
- * OCIE0A = 1 (enable interrupt on OCR0A match)
- */
 void abcRegisterSetup() {
   ADMUX = 0;
   ADMUX |= (1 << ADLAR);
   ADMUX |= (1 << REFS0);
   ADMUX |= (0 & 0x0F);
-
   ADCSRA = 0;
   ADCSRA |= (1 << ADEN);
   ADCSRA |= (1 << ADATE);
   ADCSRA |= (1 << ADIE); 
   ADCSRA |= (1 << ADPS2); 
   ADCSRA |= (1 << ADPS1);  
-  ADCSRA |= (1 << ADPS0);   
+  ADCSRA |= (1 << ADPS0);   // 128
   
   ADCSRB = 0;
-  ADCSRA |= (1 << ADSC); 
+  ADCSRA |= (1 << ADSC);  // BEGIN LEZEN
+}
+
+ISR(ADC_vect) {
+    DRAAI_DING_WAARDE = ADCH;          // LEES DING
+    LEES_DATA = 1;   // IS GELEZEN
+}
+
+int main(void) {
+    Serial.begin(9600);
+    abcRegisterSetup();
+    sei();  // ZET INTERRUPTS AAN
+    
+    while(1) {
+        if(LEES_DATA) {
+            LEES_DATA = 0; // LEES OPNIEUW
+            Serial.println(DRAAI_DING_WAARDE);
+
+        }
+    }
 }
